4d189ccd6a486e21984475d1de1e310ffc0d79dc
Whydog <joshhuang9508@gmail.com>
2024-09-27 09:46:43 +0800

    Init ESLint and Prettier to local

--- Frontend/app/index.tsx
+++ Frontend/app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
+// App entry
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style = {styles.container}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
+
+// Style sheetnpm install --save-dev eslint-config-prettier
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+});
