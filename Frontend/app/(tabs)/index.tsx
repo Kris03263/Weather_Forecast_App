9b40c0f55240c59c93ae2bc338383c8c1c74cab0
Whydog <joshhuang9508@gmail.com>
2024-09-27 17:00:31 +0800

    Add functions
    Add Widget component, time interval types, more UI design

--- Frontend/app/(tabs)/index.tsx
+++ Frontend/app/(tabs)/index.tsx
@@ -7,7 +7,9 @@ import {
   ScrollView,
   TouchableOpacity,
 } from "react-native";
+
 import { CityView } from "@/components/CItyView";
+import { Widget } from "@/components/Widget";
 
 export default function HomeScreen() {
   return (
@@ -16,19 +18,39 @@ export default function HomeScreen() {
       <View style={styles.topSection}>
         <Text style={styles.cityName}>TAIPEI, TAIWAN</Text>
         <Text style={styles.temperature}>20°C</Text>
-        <View style={styles.sunCircle} />
+        <View style={styles.weatherIcon} />
       </View>
 
       {/* 中間部分，滾動列表展示每小時預報 */}
-      <View style={styles.bodySection}>
-        <View style={styles.weatherOverallView}>
-          <CityView cityName="Taipei, Taiwan" date="09/27"></CityView>
-          <CityView cityName="New York, USA" date="09/27"></CityView>
-          <TouchableOpacity style={styles.addButton}>
-            <Text style={styles.addButtonText}>+</Text>
-          </TouchableOpacity>
+      <ScrollView style={styles.bodySection}>
+        <View style={{ gap: 20 }}>
+          <Widget style={styles.weatherOverallView}>
+            <CityView cityName="Taipei, Taiwan" date="09/27"></CityView>
+            <CityView
+              cityName="New York, USA"
+              date="09/27"
+              timeInterval_type={1}
+            ></CityView>
+            <CityView
+              cityName="Taipei, Taiwan"
+              date="09/27"
+              timeInterval_type={0}
+            ></CityView>
+            <TouchableOpacity style={styles.addButton}>
+              <Text style={styles.addButtonText}>+</Text>
+            </TouchableOpacity>
+          </Widget>
+
+          <View style={styles.horizontalDisplay}>
+            <Widget></Widget>
+            <Widget></Widget>
+          </View>
+
+          <Widget></Widget>
+          <Widget></Widget>
+          <Widget></Widget>
         </View>
-      </View>
+      </ScrollView>
     </View>
   );
 }
@@ -36,11 +58,10 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#2D2D2D",
+    backgroundColor: "#10202b",
   },
   topSection: {
     height: "30%",
-    backgroundColor: "#1C1C1C",
     justifyContent: "center",
     alignItems: "center",
     position: "relative",
@@ -56,82 +77,44 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginTop: 10,
   },
-  sunCircle: {
+  weatherIcon: {
     position: "absolute",
     right: -50,
-    top: "20%",
+    top: "50%",
     width: 150,
     height: 150,
     borderRadius: 75,
-    backgroundColor: "yellow",
+    backgroundColor: "#FFE27D",
   },
   bodySection: {
-    backgroundColor: "#FFFFFF",
+    backgroundColor: "#FFFFFFAA",
     height: "70%",
     padding: "3%",
   },
-  weatherOverallView: {
-    // flex: 0.3,
+  horizontalDisplay: {
+    flexDirection: "row",
+    justifyContent: "space-between",
     gap: 20,
-    backgroundColor: "#D9D9D9",
+  },
+  weatherOverallView: {
+    flex: 10,
+    minHeight: "auto",
+    gap: 20,
+    backgroundColor: "#FFFFFF20",
     padding: 20,
     alignItems: "center",
     borderRadius: 15,
-  },
-  weatherCityView: {
-    width: "100%",
-    overflow: "hidden",
-  },
-  subTitle: {
-    color: "black",
-    fontSize: 16,
-    marginBottom: 10,
-    fontWeight: "bold",
-  },
-  weatherScroll: {
-    flexDirection: "row",
-  },
-  weatherCard: {
-    width: 60,
-    height: 80,
-    backgroundColor: "#EAEAEA",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 8,
-    marginHorizontal: 5,
-  },
-  weatherIcon: {
-    width: 40,
-    height: 40,
-    marginBottom: 5,
-  },
-  weatherTime: {
-    fontSize: 12,
-    fontWeight: "bold",
   },
   addButton: {
     width: 40,
     height: 40,
     borderRadius: 20,
-    backgroundColor: "#C4C4C4",
+    backgroundColor: "#EAEAEA",
     justifyContent: "center",
     alignItems: "center",
   },
   addButtonText: {
     fontSize: 24,
-    color: "white",
-  },
-  bottomSection: {
-    flex: 0.4,
-    backgroundColor: "#7A7A7A",
-    flexDirection: "row",
-    justifyContent: "space-around",
-    padding: 20,
-  },
-  placeholderCard: {
-    width: 120,
-    height: 120,
-    backgroundColor: "#EAEAEA",
-    borderRadius: 8,
+    color: "#000000",
   },
 });
--- Frontend/components/CItyView.tsx
+++ Frontend/components/CItyView.tsx
@@ -1,41 +1,41 @@
-import {
-  Image,
-  StyleSheet,
-  Platform,
-  Text,
-  View,
-  ScrollView,
-  TouchableOpacity,
-} from "react-native";
+import { Image, StyleSheet, Text, View, ScrollView } from "react-native";
 
-// 定義 props 的類型
 interface CityViewProps {
   cityName: string;
   date: string;
+  timeInterval_type?: number; // 0 -> 3h | 1 -> 1D
 }
 
-export function CityView({ cityName, date }: CityViewProps) {
+export function CityView({
+  cityName,
+  date,
+  timeInterval_type = 0,
+}: CityViewProps) {
+  const timeInterval_map = [
+    [
+      "00:00",
+      "03:00",
+      "06:00",
+      "09:00",
+      "12:00",
+      "15:00",
+      "18:00",
+      "21:00",
+      "24:00",
+    ],
+    ["Sun.", "Mon.", "Tue.", "Wed.", "Thr.", "Fri.", "Sat."],
+  ];
+
   return (
     <View style={styles.weatherCityView}>
       <Text style={styles.subTitle}>
         {cityName} ({date})
       </Text>
       <ScrollView horizontal style={styles.weatherScroll}>
-        {/* 每小時的預報卡片 */}
-        {[
-          "00:00",
-          "03:00",
-          "06:00",
-          "09:00",
-          "12:00",
-          "15:00",
-          "18:00",
-          "21:00",
-          "24:00",
-        ].map((time, index) => (
+        {timeInterval_map[timeInterval_type].map((time, index) => (
           <View key={index} style={styles.weatherCard}>
             <Image
-              // source={require("./cloud.png")}
+              // source={require("./cloud.png")} // require weather image
               style={styles.weatherIcon}
             />
             <Text style={styles.weatherTime}>{time}</Text>
@@ -77,17 +77,5 @@ const styles = StyleSheet.create({
   weatherTime: {
     fontSize: 12,
     fontWeight: "bold",
-  },
-  addButton: {
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-    backgroundColor: "#C4C4C4",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  addButtonText: {
-    fontSize: 24,
-    color: "white",
   },
 });
